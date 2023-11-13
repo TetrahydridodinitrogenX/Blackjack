dde317b9663bb59324977b720886a50698e94a42
Andrew <73075564+TetrahydridodinitrogenX@users.noreply.github.com>
2023-11-13 19:21:00 +0000

    vghjvk

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// Global Variables
+int globalTextDelay = 50;
+string[] cards = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+
+// Init Screen
+string titleText = "Blackjack";
+    foreach(char c in titleText){
+        Console.Write(c);
+            await Task.Delay(globalTextDelay);
+    } Console.ReadLine();
+
