50833bb25ee980844f3bf8dd426a003e1830004c
TetrahydridodinitrogenX <73075564+TetrahydridodinitrogenX@users.noreply.github.com>
2023-11-14 18:00:31 -0500

    send

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿// Global Variables
 int globalTextDelay = 50;
 int textSpacing = 2;
-int chips = 0;
+// int chips = 0;
 string[] cards = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
 cards[10] = "10";
 cards[11] = "10";
@@ -19,8 +19,9 @@ string titleText = "Blackjack";
 Random rnd = new Random();
     for(int i = 0; i < 13; i++){int randomVal = rnd.Next(1, 13); Console.WriteLine(cards[randomVal]);} await Task.Delay(100); Console.Clear(); Console.ReadLine(); 
 
-int initChips = chips + 100;
-Console.WriteLine($"You Have" + initChips, " chips");
+// chips = chips + 100;
+// Console.Write($"You Have" + chips);
+// Console.Write(" Chips");
 
 Random rand = new Random();
     int playerCardOneRand = rand.Next(1, 13);
@@ -39,43 +40,9 @@ Console.WriteLine("Your Card: " + playerCardOne);
 Console.WriteLine("Dealers Card: " + dealerCardOne);
     await Task.Delay(100);
 
-Choice:
-Console.Write("1: Hit | 2: Stand");
+Console.WriteLine("");
+Console.WriteLine("1: Hit | 2: Stand");
 int choiceSelect = Convert.ToInt32(Console.ReadLine());
 for(int i = 0; i < textSpacing; i++){Console.WriteLine("");}
 
-    if(choiceSelect == 1){ // HIT
-        for(int i = 0; i < textSpacing; i++){Console.WriteLine("");}
-            Console.WriteLine("You Hit!"); Console.WriteLine("");
-        Random randSnd = new Random();
-            int playerCardTwoRand = rand.Next(1, 13);
-            int playerCardTwo = int.Parse(cards[playerCardTwoRand]);
-        Console.WriteLine("Your Second Card: " + playerCardTwo);
-            int cardTotal = playerCardOne + playerCardTwo;
-        Console.WriteLine("Your Total = " + cardTotal);
-            if(playerCardOne == playerCardTwo){ // SPLIT
-                for(int i = 0; i < textSpacing; i++){Console.WriteLine("");}
-                    Console.WriteLine("Same Card! Split? (1 = Y | 2 = N)");
-                        int splitSelect = Convert.ToInt32(Console.ReadLine());
-                        for(int i = 0; i < textSpacing; i++){Console.WriteLine("");}
-                    if(splitSelect == 1){
-
-                    } else if(splitSelect == 0){
-
-                    }
-            } else if(cardTotal == 21){ //BLACKJACK
-                for(int i = 0; i < textSpacing; i++){Console.WriteLine("");}
-                    Console.WriteLine("Player Blackjack!");
-                        goto Start;
-            } else if(cardTotal >= 21){ // LOSS
-                for(int i = 0; i < textSpacing; i++){Console.WriteLine("");}
-                    Console.WriteLine("You Went Over 21!");
-                        goto Start;
-            }
-    } else if(choiceSelect == 2){ // STAND
-            for(int i = 0; i < textSpacing; i++){Console.WriteLine("");}
-    } else if(choiceSelect != 1 && choiceSelect != 2){ // INVALID
-        for(int i = 0; i < textSpacing; i++){Console.WriteLine("");}
-            Console.WriteLine("Invalid!");
-                goto Choice;
-    }
+Console.Read();
