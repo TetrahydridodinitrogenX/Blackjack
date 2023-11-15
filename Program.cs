bf6241ce00c742f419ee457d0624f3f91eae8e6c
Andrew <73075564+TetrahydridodinitrogenX@users.noreply.github.com>
2023-11-15 18:11:20 +0000

    yeah

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿// Global Variables
+using System.Globalization;
+
 int globalTextDelay = 50;
 int textSpacing = 2;
 // int chips = 0;
@@ -18,8 +20,8 @@ string titleText = "Blackjack";
             await Task.Delay(globalTextDelay);
     } Console.ReadLine(); Console.Clear();
 
-Random rnd = new Random();
-    for(int i = 0; i < 13; i++){int randomVal = rnd.Next(1, 13); Console.WriteLine(cards[randomVal]);} await Task.Delay(100); Console.Clear(); Console.ReadLine(); 
+// Random rnd = new Random();
+//     for(int i = 0; i < 13; i++){int randomVal = rnd.Next(1, 13); Console.WriteLine(cards[randomVal]);} await Task.Delay(100); Console.Clear(); Console.ReadLine(); 
 
 // chips = chips + 100;
 // Console.Write($"You Have" + chips);
@@ -32,9 +34,10 @@ Random rand = new Random();
     int dealerCardTwoRand = rand.Next(1, 13);
     int dealerCardOne = int.Parse(cards[dealerCardOneRand]);
     int dealerCardTwo = int.Parse(cards[dealerCardTwoRand]);
+    int dealerInitTotal = dealerCardOne + dealerCardTwo;
         if(dealerCardOne + dealerCardTwo == 21){
             for(int i = 0; i < textSpacing; i++){Console.WriteLine("");}
-                Console.WriteLine("Dealer Blackjack!");
+                Console.WriteLine("Dealer Blackjack!"); await Task.Delay(100); Console.ReadLine();
                     goto Start;
         }
 
@@ -53,7 +56,7 @@ for(int i = 0; i < textSpacing; i++){Console.WriteLine("");}
     } else if(choiceSelect == 2){ // STAND
         for(int i = 0; i < textSpacing; i++){Console.WriteLine("");}
             Console.WriteLine("You Stand!");
-        goto CONT;
+        goto STD;
     } else if(choiceSelect != 1 && choiceSelect != 2){
         for(int i = 0; i < textSpacing; i++){Console.WriteLine("");}
             Console.WriteLine("Invalid!"); Console.Clear();
@@ -72,12 +75,57 @@ Hit:
     
     if(playerCardTotal == 21){
         for(int i = 0; i < textSpacing; i++){Console.WriteLine("");}
-
+            Console.WriteLine("Blackjack!"); await Task.Delay(100); Console.ReadLine();
+        goto Start;
     }
 
-CONT:
+    for(int i = 0; i < textSpacing; i++){Console.WriteLine("");}
+        Console.WriteLine("Hit Again?"); Console.WriteLine("");
+    int hitAgain = Convert.ToInt32(Console.ReadLine());
+        if(hitAgain == 1){
+            Console.WriteLine("You Hit Again!");
+                Random trdCard = new Random();
+                int playerCardThreeRand = trdCard.Next(1, 13);
+                int playerCardThree = int.Parse(cards[playerCardThreeRand]);
+            Console.WriteLine("Your Third Card: " + playerCardThree);
+                int newPlayerCardTotal = playerCardOne + playerCardTwo + playerCardThree;
+            Console.WriteLine("Your Total = " + newPlayerCardTotal);
+                if(newPlayerCardTotal > 21){
+                    for(int i = 0; i < textSpacing; i++){Console.WriteLine("");}
+                        Console.WriteLine("You Went Over 21!"); await Task.Delay(100); Console.ReadLine();
+            } else if(hitAgain == 2){
+                await Task.Delay(100); goto STD;
+            }
+                if(newPlayerCardTotal < 21){
+                    for(int i = 0; i < textSpacing; i++){Console.WriteLine("");}
+                        Console.WriteLine("Hit Again?");
+                    int thirdHit = Convert.ToInt32(Console.ReadLine());
+                        if(thirdHit == 1){
+            Console.WriteLine("You Hit Again!");
+                Random fourthCard = new Random();
+                int playerCardFourRand = fourthCard.Next(1, 13);
+                int playerCardFour = int.Parse(cards[playerCardFourRand]);
+            Console.WriteLine("Your Third Card: " + playerCardThree);
+                int PlayerCardTotal3 = playerCardOne + playerCardTwo + playerCardThree + playerCardFour;
+            Console.WriteLine("Your Total = " + PlayerCardTotal3);
+                if(PlayerCardTotal3 > 21){
+                    for(int i = 0; i < textSpacing; i++){Console.WriteLine("");}
+                        Console.WriteLine("You Went Over 21!"); await Task.Delay(100); Console.ReadLine();
+                }
+                } else if(thirdHit == 2){
+                    await Task.Delay(100); goto STD;
+                }
+            }
+        }
 
+STD:
+for(int i = 0; i < textSpacing; i++){Console.WriteLine("");}
+    Console.WriteLine("You Stand!"); await Task.Delay(100);
+// if(dealerInitTotal == 17){
+// }
+// Random finalDealer = new Random();
+//     int dealerNextCard = finalDealer.Next(1, 13);
+//     int dealerCardThree = int.Parse(cards[dealerNextCard]);
 
-
-Console.Read();
+// Console.Read();
 // ADD BLACKJACK FEATURE DOWN HERE
